e9243a5fff5380065832fff077fd04ecc5cccb12
akshat2048 <77075876+akshat2048@users.noreply.github.com>
2021-10-11 20:59:43 -0500

    scrapes judge data
    Scrapes judge data

--- utils/judgeAnalysis.py
+++ utils/judgeAnalysis.py
@@ -1,3 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+
+def analysis(URL: str) -> str:
+    r = requests.get(URL)
+    soup = BeautifulSoup(r.text, "html.parser")
+
+    judgeData = []
+    judges = soup.find_all('div', attrs = {'class': 'bordertop borderbottom'})
+
+    for judge in judges:
+        judgeDataDict = {}
+        judgeDataDict['name'] = str(judge.find('span', attrs = {'class': 'threetenths semibold bluetext'}).string).replace('\t', '').replace('\n', '')
+        judgeDataDict['school'] = str(judge.find('span', attrs = {'class': 'fourtenths'}).string).replace('\t', '').replace('\n', '')
+        judgeDataDict['paradigm'] = str(judge.parent.find('div', attrs = {'class': 'paradigm hidden'}).text).replace('\t', '')
+        judgeData.append(judgeDataDict)
+
+        #Debugging
+        print("Name:", judgeDataDict['name'])
+        print("School:", judgeDataDict['name'])
+        print("Paradigm:", judgeDataDict['paradigm'])
+
+
+analysis("https://www.tabroom.com/index/tourn/paradigms.mhtml?category_id=53735&tourn_id=21009")
