4402d3f2fb22cac8e999ec4b102b7ddc27e8df2d
Samarth Chitgopekar <67826352+http-samc@users.noreply.github.com>
2024-06-30 12:31:05 -0700

    Update const.py

--- shared/const.py
+++ shared/const.py
@@ -1,5 +1,5 @@
 import os
 
 # API_BASE = "http://localhost:8080/core" if os.environ['RUNTIME'] == "local" else f"{os.environ['REMOTE_API_URL']}/core"
-# API_BASE = os.environ['REMOTE_API_URL'] + '/core'
-API_BASE = "http://localhost:8080/core"
+API_BASE = os.environ['REMOTE_API_URL'] + '/core'
+# API_BASE = "http://localhost:8080/core"
