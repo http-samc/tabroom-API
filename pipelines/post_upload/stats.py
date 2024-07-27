3b76acb9a1d10ebba712136973c17198a9356242
Samarth Chitgopekar <67826352+http-samc@users.noreply.github.com>
2024-07-27 14:44:28 -0700

    Reduced posthog calls

--- pipelines/post_upload/stats.py
+++ pipelines/post_upload/stats.py
@@ -189,7 +189,7 @@ def _update_scoped_stats(job_id: int | None, season: int, circuit: int):
     #         'judge_2_index': judge_2_speaks,
     #     }, f)
     for i, (teamId, rounds) in enumerate(team_2_rounds.items()):
-        lprint(job_id, "Info", message=f"Updating {i+1}/{len(team_2_rounds.items())}")
+        # lprint(job_id, "Info", message=f"Updating {i+1}/{len(team_2_rounds.items())}")
         x_wp = []
         # We don't have an expWp for ALL rounds (eg. bye)
         wins_with_exp_wp_recorded = 0
